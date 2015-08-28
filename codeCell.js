3348cd49af2970d79335f7b171cd15ce37d75027
Chase Meadors <c.ed.mead@gmail.com>
2015-08-28 00:17:21 -0500

    Implemented the rest of the cell types' logic

--- codeCell.js
+++ codeCell.js
@@ -16,6 +16,22 @@ var codeCell = codeCell || {},
         },
 
         /**
+         CrossConveyor
+
+         Moves execution UP if the previous facing was UP or DOWN
+         Moves execution RIGHT if the previous facing was RIGHT or LEFT
+         Makes no changes to the tape
+         (This cell, like conveyor, will handle orientation implicitly by letting the default orientation be ^>)
+         */
+        CrossConveyor: function CrossConveyor(head, previousFacing) {
+            if (previousFacing == program.directions.UP || previousFacing == program.directions.DOWN) {
+                return [false, null, program.directions.UP];
+            } else if (previousFacing == program.directions.UP || previousFacing == program.directions.DOWN) {
+                return [false, null, program.directions.RIGHT]
+            }
+        },
+
+        /**
          BranchBR
 
          If head is RED, pop tape and move LEFT
@@ -32,7 +48,47 @@ var codeCell = codeCell || {},
             }
 
             return [false, null, program.directions.UP];
-        }
+        },
+
+        /**
+         BranchGY
+
+         If head is GREEN, pop tape and move LEFT
+         If head is YELLOW, pop tape and move RIGHT
+         Otherwise, don't pop and move UP
+         */
+        BranchBR: function BranchGY(head) {
+            if (head === core.GREEN) {
+                return [true, null, program.directions.LEFT];
+            }
+
+            if (head === core.YELLOW) {
+                return [true, null, program.directions.RIGHT];
+            }
+
+            return [false, null, program.directions.UP];
+        },
+
+        /**
+         Writers
+         Append <color>
+         Move UP
+         */
+        WriteB: function WriteB(head) {
+            return [false, core.BLUE, program.directions.UP];
+        },
+
+        WriteR: function WriteR(head) {
+            return [false, core.RED, program.directions.UP];
+        },
+
+        WriteG: function WriteG(head) {
+            return [false, core.GREEN, program.directions.UP];
+        },
+
+        WriteY: function WriteY(head) {
+            return [false, core.YELLOW, program.directions.UP];
+        },
 
     };
 
