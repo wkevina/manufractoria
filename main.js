24cd2ba68917367724f0ba20bcc344b2395cc78b
Chase Meadors <c.ed.mead@gmail.com>
2015-08-29 04:08:01 -0500

    Fixed graphics
    Added customToTransformString function with sketchy hack inside

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+var customToTransformString = function (matrix) {
+    var E = "";
+    var s = matrix.split();
+    if (!+s.shear.toFixed(9)) {
+        s.scalex = +s.scalex.toFixed(4);
+        s.scaley = +s.scaley.toFixed(4);
+        s.rotate = +s.rotate.toFixed(4);
+        return  (s.dx || s.dy ? "t" + [+s.dx.toFixed(4), +s.dy.toFixed(4)] : E) + 
+                (s.scalex != 1 || s.scaley != 1 ? "s" + [s.scalex, s.scaley] : E) +
+                (s.rotate ? "r" + [s.scalex*s.scaley < 0 ? 360 - s.rotate.toFixed(4) : +s.rotate.toFixed(4)] : E);
+
+        // This is the same as what Snap.svg does by default with two major differences (original is in matrix.js)
+        //
+        // 1. No ",0,0" is appended to the rotate and scale strings, so they will now default to the center of the element
+        //
+        // 2. The complicated one: If we have been mirrored in either x or y but not both (i.e., either scalex or scaley is 
+        //    negative, but not both (just test if their product is negative)), our interpretation of "rotate" changes. 
+        //    in particular, in the mirrored case, rotate needs to be interpreted as going "backward" or "clockwise". So,
+        //    to get the actual correct rotation in this case, we subtract it from 360. Whether or not the original behavior is
+        //    actually incorrect on the part of Snap needs more study. 
+
+    } else {
+        return "m" + [matrix.get(0), matrix.get(1), matrix.get(2), matrix.get(3), matrix.get(4), matrix.get(5)];
+    }
+};
+
+
 var core = core || {},
     program = program || {},
     interpreter = interpreter || {},
@@ -207,42 +234,8 @@ var core = core || {},
 
             grid.drawGrid();
 
-            var p = new program.Program(10, 10);
-            p.setStart(5, 0);
-            p.setCell(5, 9, "End");
-
-            for (var x = 0; x < 10; ++x) {
-                for (var y = 1; y < 9; ++y) {
-
-                    var choice = Math.floor(Math.random()*4);
-
-                    switch (choice) {
-                    case 0:
-                        p.setCell(x, y, "BranchBR");
-                        break;
-                    case 1:
-                        p.setCell(x, y, "BranchBR");
-                        break;
-                    case 2:
-                        p.setCell(x,y, "BranchGY");
-                        break;
-                    case 3:
-                        p.setCell(x,y, "Conveyor");
-                        break;
-                    default:
-                        p.setCell(x, y, "BranchBR");
-                    }
-
-                    p.getCell(x, y).orientation = tmath.Mat2x2.MROT1();
-                }
-            }
-
-            // p.setCell(5, 5, "BranchBR");
-            // p.setCell(4, 5, "BranchBR");
-            // p.setCell(3, 5, "BranchBR");
-            // p.setCell(2, 5, "BranchBR");
-
-            // p.setCell(6, 5, "Conveyor");
+            //var p = program.readLegacyProgramString("lvl=32&code=c12:4f3;c12:5f3;p12:6f0;c11:6f3;c11:7f3;c11:8f3;c11:9f3;c11:10f3;c11:11f2;&ctm=N1;N2;bbr:x|rrb:x;9;3;1;");
+            var p = program.readLegacyProgramString("lvl=32&code=c10:4f0;c11:4f1;c12:4f2;c13:4f3;p10:5f0;p11:5f1;p12:5f2;p13:5f3;p10:6f4;p11:6f5;p12:6f6;p13:6f7;q10:7f0;q11:7f1;q12:7f2;q13:7f3;q10:8f4;q11:8f5;q12:8f6;q13:8f7;&ctm=N1;N2;bbr:x|rrb:x;9;3;0;");
 
             var myInterpreter = new interpreter.Interpreter();
             myInterpreter.setProgram(p);
@@ -288,6 +281,7 @@ var core = core || {},
 
 
     function drawProgram(paper, program, grid) {
+        console.log(program);
         for (var x = 0; x < program.cols; ++x) {
             for (var y = 0; y < program.rows; ++y) {
                 var c = program.getCell(x, y);
@@ -302,23 +296,17 @@ var core = core || {},
 
                         var group = paper.g(image);
 
-                        console.log(group.getBBox());
-                        console.log(group.transform());
-
-                        
                         var corner = grid.getCellMatrix(x, y, true)
                                 .toTransformString()
                                 .toUpperCase();
 
                         var o = c.orientation;
                         
-                        var transform = Snap.matrix(o.a, o.b, o.c, o.d, 28, 28);
-                        transform.add(Snap.matrix().translate(-28, -28));
+                        var transform = Snap.matrix(o.a, o.b, o.c, o.d, 0, 0);
+                        var tstring = customToTransformString(transform);
 
-                        console.log(transform.toTransformString());
                         group.transform(
-                            transform.toTransformString() + 
-                            corner
+                            tstring + corner
                         );
 
                         var marker = paper.circle(0, 0, 2);
