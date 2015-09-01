41b41d291cc4ba512a1d7e23dd4c92115581d1f2
Kevin Ward <kevinward@fescorp.com>
2015-09-01 17:05:40 -0500

    Should be working?

--- loader.js
+++ loader.js
@@ -65,11 +65,136 @@
 
 */
 
-
-
-
-var loader = loader || {};
+var loader = loader || {},
+    codeCell = codeCell || {};
 
 (function() {
+    function isTape(t) {
+        // Ensure tapeDesc only contains B,R,G,Y
+        var invalidChars = t.match(/[^RGBY]/);
+        if (invalidChars != null)
+            return false;
+        return true;
+    }
+
+    loader.isTape = isTape;
+
+    function isOrientation(o) {
+        var index = ["ID", "ROT1", "ROT2", "ROT3", "MID", "MROT1", "MROT2", "MROT3"].indexOf(o);
+        if (index == -1)
+            return false;
+        return true;
+    }
+
+    loader.isOrientation = isOrientation;
+
+    function isCellType(t) {
+        var validTypes = Object.keys(codeCell.codeCells);
+        var index = validTypes.indexOf(t);
+        if (index == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    loader.isCellType = isCellType;
+
+    function isCoordinate(c) {
+        return !isNaN(c);
+    }
+
+    loader.isCoordinate = isCoordinate;
+
+    function hasAll(ob, required) {
+        var keys = Object.keys(ob);
+        return required.every(_.partial(_.contains, keys, _));
+    }
+
+    loader.hasAll = hasAll;
+
+    function isCellDesc(cellDesc) {
+
+        if (!hasAll(cellDesc, ["type", "x", "y", "orientation"])) {
+            return false;
+        }
+
+        return allTrue([
+            isCellType(cellDesc.type),
+            isOrientation(cellDesc.orientation),
+            isCoordinate(cellDesc.x),
+            isCoordinate(cellDesc.y)
+        ]);
+    }
+
+    loader.isCellDesc = isCellDesc;
+
+    function isEndpoint(e) {
+        if (!hasAll(e, ["orientation", "x", "y"])) {
+            return false;
+        }
+
+        return allTrue([
+            isOrientation(e.orientation),
+            isCoordinate(e.x),
+            isCoordinate(e.y)
+        ]);
+    }
+
+    loader.isEndpoint = isEndpoint;
+
+    function allTrue(l) {
+        return l.every(function(p) {
+            return Boolean(p);
+        });
+    }
+
+    loader.allTrue = allTrue;
+
+    function isProgram(p) {
+        if (!hasAll(p, ["start", "end", "cols", "rows", "cells"])) {
+            return false;
+        }
+
+        return allTrue([
+            isCoordinate(p.cols),
+            isCoordinate(p.rows),
+            p.cells.every(isCellDesc),
+            isEndpoint(p.start),
+            isEndpoint(p.end)
+        ]);
+    }
+
+    loader.isProgram = isProgram;
+
+    function isValid(level) {
+        if (!hasAll(level, ["title", "tape", "program"])) {
+            return false;
+        }
+
+        return allTrue([
+            level.tape.every(isTape),
+            isProgram(level.program)
+        ]);
+    }
+
+    loader.isValid = isValid;
 
 })();
+
+// program-description:
+
+//  {
+//  	cols: Number,
+//  	rows: Number,
+// 	cells: [ cell-description1, cell-description2 ],
+//  	start: {
+//  		x: Number,
+// 		y: Number,
+//  		orientation: orientation-description
+// 	},
+// 	end: {
+// 		x: Number,
+// 		y: Number,
+//  		orientation: orientation-description
+// 	}
+//  }
