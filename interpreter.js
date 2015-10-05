61724ac205ebb016954d38d9fef69a216201065b
Chase Meadors <c.ed.mead@gmail.com>
2015-10-05 14:35:21 -0500

    ES6-ified Intepreter

--- interpreter.js
+++ interpreter.js
@@ -7,119 +7,120 @@ import codeCell from "codeCell";
 import tmath from "tmath";
 import core from "core";
 
-var Interpreter = function() {
-    this.tape = new core.Tape();
-    this.program = null;
+class Interpreter {
+    constructor() {
+        this.tape = new core.Tape();
+        this.program = null;
 
-    this.accept = false;
-    this.running = false;
+        this.accept = false;
+        this.running = false;
 
-    this.position = new tmath.Vec2(0, 0);
-    this.facing = program.directions.UP;
+        this.position = new tmath.Vec2(0, 0);
+        this.facing = program.directions.UP;
 
-    this.cycles = 0;
-};
+        this.cycles = 0;
+    }
 
-Interpreter.prototype.setProgram = function(program) {
-    this.program = program;
-};
+    setProgram(program) {
+        this.program = program;
+    }
 
-Interpreter.prototype.setTape = function(tape) {
-    this.tape = tape;
-};
+    setTape(tape) {
+        this.tape = tape;
+    }
 
-Interpreter.prototype.start = function() {
-    this.accept = false;
-    this.running = true;
-    this.cycles = 0;
+    start() {
+        this.accept = false;
+        this.running = true;
+        this.cycles = 0;
 
-    // Go to the start
-    for (var x = 0; x < this.program.cols; x ++) {
-        for (var y = 0; y < this.program.rows; y ++) {
-            if (this.program.getCell(x, y).type == "Start") {
-                this.position.x = x;
-                this.position.y = y;
+        // Go to the start
+        for (let x of this.program.cols) {
+            for (let y of this.program.rows) {
+                if (this.program.getCell(x, y).type == "Start") {
+                    this.position.x = x;
+                    this.position.y = y;
+                }
             }
         }
+
+        // Face +y;
+        this.facing = program.directions.UP;
     }
 
-    // Face +y;
-    this.facing = program.directions.UP;
-};
+    convertDirectionGlobalToCell(d, cell) {
+        return cell.orientation.apply(d);
+    }
 
-Interpreter.prototype.convertDirectionGlobalToCell = function(d, cell) {
-    return cell.orientation.apply(d);
-};
+    convertDirectionCellToGlobal(d, cell) {
+        return cell.orientation.invert().apply(d);
+    }
 
-Interpreter.prototype.convertDirectionCellToGlobal = function(d, cell) {
-    return cell.orientation.invert().apply(d);
-};
+    // Returns tuple [pop tape head or not (bool), symbol to push (maybe null), new facing direction]
+    evalCell(cell, tapeHead) {
 
-// Returns tuple [pop tape head or not (bool), symbol to push (maybe null), new facing direction]
-Interpreter.prototype.evalCell = function(cell, tapeHead) {
+        let cellFunc = codeCell.codeCells[cell.type];
 
-    var cellFunc = codeCell.codeCells[cell.type];
+        let result = null;
 
-    var result = null;
+        if (cellFunc) {
+            if (cell.type == "CrossConveyor") {
+                // Special case. Convert this.facing into cell coordinates for CrossConveyor's function:
+                let cellFacing = this.convertDirectionGlobalToCell(this.facing, cell);
+                result = cellFunc(tapeHead, cellFacing);
+            } else {
+                // No knowledge of current facing needed
+                result = cellFunc(tapeHead);
+            }
 
-    if (cellFunc) {
-        if (cell.type == "CrossConveyor") {
-            // Special case. Convert this.facing into cell coordinates for CrossConveyor's function:
-            var cellFacing = this.convertDirectionGlobalToCell(this.facing, cell);
-            result = cellFunc(tapeHead, cellFacing);
+            // Convert cell's returned direction into global direction
+            result[2] = this.convertDirectionCellToGlobal(result[2], cell);
+            return result;
+        }
+
+        console.log("Invalid cell type.");
+
+        return [false, null, program.directions.UP];
+    }
+
+    step() {
+
+        if (!this.running) return;
+
+        // Get state
+        let cell = this.program.getCell(this.position.x, this.position.y);
+        let head = this.tape.head();
+
+        // Check if done
+        if (cell.type == "Empty" || (cell.type == "Start" && this.cycles > 0)) {
+            this.running = false;
+            this.accept = false;
+        } else if (cell.type == "End") {
+            this.running = false;
+            this.accept = true;
         } else {
-            // No knowledge of current facing needed
-            result = cellFunc(tapeHead);
+
+            // Evaluate cell
+            let result = this.evalCell(cell, head);
+
+            // Perform result
+            if (result[0]) {
+                this.tape.pop();
+            }
+
+            if (result[1] != null) {
+                this.tape.append(result[1]);
+            }
+
+            this.facing = result[2];
+
+            // Move 'facing' direction:
+            this.position = this.position.add(this.facing);
+            this.cycles += 1;
         }
 
-        // Convert cell's returned direction into global direction
-        result[2] = this.convertDirectionCellToGlobal(result[2], cell);
-        return result;
     }
+}
 
-    console.log("Invalid cell type.");
-
-    return [false, null, program.directions.UP];
-};
-
-Interpreter.prototype.step = function() {
-
-    if (!this.running) return;
-
-
-
-    // Get state
-    var cell = this.program.getCell(this.position.x, this.position.y);
-    var head = this.tape.head();
-
-    // Check if done
-    if (cell.type == "Empty" || (cell.type == "Start" && this.cycles > 0)) {
-        this.running = false;
-        this.accept = false;
-    } else if (cell.type == "End") {
-        this.running = false;
-        this.accept = true;
-    } else {
-
-        // Evaluate cell
-        var result = this.evalCell(cell, head);
-
-        // Perform result
-        if (result[0]) {
-            this.tape.pop();
-        }
-
-        if (result[1] != null) {
-            this.tape.append(result[1]);
-        }
-
-        this.facing = result[2];
-
-        // Move 'facing' direction:
-        this.position = this.position.add(this.facing);
-        this.cycles += 1;
-    }
-
-};
 
 interpreter.Interpreter = Interpreter;
